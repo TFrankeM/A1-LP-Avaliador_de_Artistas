1b2cc8208b8d9eed8a7f66f6d9f2d2fe1403fa1e
Teago <thiago.poganski@gmail.com>
2022-10-04 16:50:35 -0300

    Gráficos iniciais 2

--- graficos.py
+++ graficos.py
@@ -4,16 +4,25 @@ import perguntas as pg
 import matplotlib.pyplot as plt
 
 data1 = pd.read_excel('BD - Skillet.xlsx')
+for linha, duracao in enumerate(data1['Duração']):
+    data1['Duração'][linha] = int(duracao[0:2])*60 + int(duracao[3:5])
 mais_ouvidas, menos_ouvidas = pg.musica_ouvida_album(data1)
-#mais_ouvidas = mais_ouvidas.head(3)
+mais_longas, mais_curtas = pg.musica_ouvida_carreira(data1)
+
+
 
 sns.set_theme(style="darkgrid")
 
-p11 = sns.catplot(data=mais_ouvidas, x="Popularidade", y="Músicas",kind="bar")
-p11.set_axis_labels("Popularidade","")
+p111 = sns.catplot(data=mais_ouvidas, x="Popularidade", y="Músicas",kind="bar")
+p111.set_axis_labels("Popularidade","")
 plt.title("Músicas mais populares de cada álbum")
 
-p12 = sns.catplot(data=menos_ouvidas, x="Popularidade", y="Músicas",kind="bar")
-p12.set_axis_labels("Popularidade","")
+p112 = sns.catplot(data=menos_ouvidas, x="Popularidade", y="Músicas",kind="bar")
+p112.set_axis_labels("Popularidade","")
 plt.title("Músicas menos populares de cada álbum")
+
+p121 = sns.catplot(data=mais_longas, x="Duração", y="Músicas",kind="bar", hue="Álbuns")
+p121.set_axis_labels("Duração","")
+plt.title("Músicas mais longas de cada álbum")
+
 plt.show()

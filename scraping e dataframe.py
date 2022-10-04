4f6cf92467b4594b92d2230be033a34e887f1760
Ricael_Daniel <101849254+RicaelDaniel@users.noreply.github.com>
2022-10-03 22:42:50 -0300

    Fazer documentação de cada uma das funções

--- scraping e dataframe.py
+++ scraping e dataframe.py
@@ -6,14 +6,13 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
 
-# Função scrapping que vai pegar os títulos dos álbuns e retorna uma lista com os títulos.
 titulo_dos_albuns = []
 
 def TituloAlbuns(link):
     """
-    TituloAlbuns 
-    :param tracks_id: Lista com os id's das músicas
-    :return: não retorna nada, epenas executa a função Popularidade para cada id de música
+    TituloAlbuns Faz o scraping dos títulos dos álbuns da banda skillet no site vagalume.
+    :param link: Link do site vagalume que vai ser utilizado no scraping.
+    :return: Retorna uma lista com os títulos dos álbuns da banda. 
     """
     site = requests.get(link).content
     soup = BeautifulSoup(site, 'html.parser')
@@ -27,14 +26,19 @@ def TituloAlbuns(link):
 TituloAlbuns("https://www.vagalume.com.br/skillet/discografia/")      
 
 
-# Função que faz scraping do título das músicas e retorna uma lista com os títulos
 titulo_das_musicas = []
 
-def TituloMusicas(link): 
+def TituloMusicas(link):
+    """
+    TituloMusicas Faz o scraping dos títulos das músicas de cada álbum da banda skillet.
+    :param link: Link do site vagalume que vai ser utilizado no scraping.
+    :return: Retorna uma lista com os títulos das músicas de cada álbum da banda. 
+    """ 
     site = requests.get(link).content
     soup = BeautifulSoup(site, 'html.parser')
     titulos_musicas = soup.find_all("a", attrs={"class": "nameMusic"})
     
+    # Colocando o nome da música que falta, e retirandao partes dos nomes que temos nos nomes das músicas que não interessa.
     for titulo_de_musica in titulos_musicas:
         if titulo_de_musica.text == '':
             titulo_das_musicas.append('Vapor')
@@ -47,51 +51,74 @@ def TituloMusicas(link):
                            
         else:
             titulo_das_musicas.append(titulo_de_musica.text)
+    
     return titulo_das_musicas    
 TituloMusicas("https://www.vagalume.com.br/skillet/discografia/")
 
-# Função que faz o scraping do ano de lançamento de cada álbum e retorna uma lista com os anos.
+
 ano_lancamento_albuns = []
 
-def AnoLancamentoAlbuns(link): 
+def AnoLancamentoAlbuns(link):
+    """
+    AnoLancamentoAlbuns Faz o scraping dos anos de lançamento dos álbuns da banda skillet.
+    :param link: Link do site vagalume que vai ser utilizado no scraping.
+    :return: Retorna uma lista com os anos de lanlamentos dos álbuns. 
+    """  
     site = requests.get(link).content
     soup = BeautifulSoup(site, 'html.parser')
     anos_dos_lancamentos = soup.find_all("p",attrs={"class": "albumYear"})
     
     for ano_de_lancamento in anos_dos_lancamentos:
         ano_lancamento_albuns.append(ano_de_lancamento.text[0:4])
+
     return ano_lancamento_albuns
 AnoLancamentoAlbuns("https://www.vagalume.com.br/skillet/discografia/")
 
 
-# Função que faz o scraping do número de faixas que cada álbum contém e retorna uma lista com a quantidade.
 num_faixas = []
 
 def QuantidadeDeFaixas(link):
+    """
+    QuantidadeDeFaixas Faz o scraping da quantidade de faixas dos álbuns da banda skillet.
+    :param link: Link do site vagalume que vai ser utilizado no scraping.
+    :return: Retorna uma lista com as quantidades de faixas de cada álbum álbuns. 
+    """  
     site = requests.get(link).content
     soup = BeautifulSoup(site, 'html.parser')
     quantidade_de_faixas = soup.find_all("p",attrs={"class": "track"})
     
     for quantidade in quantidade_de_faixas:
         num_faixas.append(quantidade.text[2:4])
+
     return num_faixas
 QuantidadeDeFaixas("https://www.vagalume.com.br/skillet/discografia/")
 
 
-# Preciso Dar alguma descrição para essas duas funções depois!!!!!!!!!!!!!!!
 titulo_dos_albuns_repetidos = []
 
 def RepetindoAlbuns(titulo):
+    """
+    RepetindoAlbuns repeti o nome do álbum em uma quantidade de vezes igual ao número de faixas que tem o álbum.
+    :param titulo: lista com título de todos os álbuns da banda.
+    :return: Retorna uma lista com os títulos dos álbuns repetidos. 
+    """  
     for posicao, nome in enumerate(titulo[::]):
         vezes = int(num_faixas[posicao])
         for quantidade in range(0, vezes):
             titulo_dos_albuns_repetidos.append(nome)
+
     return titulo_dos_albuns_repetidos
 RepetindoAlbuns(titulo_dos_albuns)
 
+
 ano_lancamento_albuns_repetidos = []
 
-def RepetindoAnos(anos):       
+def RepetindoAnos(anos):
+    """
+    RepetindoAnos repeti o ano de lançamento dos álbuns um número de vezes igual a quantidade de faixas que temos o álbum da banda skillet.
+    :param anos: lista com os anos de lançamento dos álbuns.
+    :return: Retorna uma lista com os anos de lançamentos repetidos. 
+    """         
     for posicao, nome in enumerate(anos[::]):
         vezes_ano = int(num_faixas[posicao])
         for quantidade in range(0,vezes_ano):
@@ -101,10 +128,14 @@ def RepetindoAnos(anos):
 RepetindoAnos(ano_lancamento_albuns)
 
 
-# Função para scraping do link do site que contém as páginas de todas as letras das músicas e adicionar em uma lista.
 link_para_as_letras = []
 
 def LinkLetraMusicas(link):
+    """
+    LinkLetraMusicas pega no site vagalume o link para todas as letras das músicas da banda skillet.
+    :param link: Link do site vagalume onde será feito o scraping.
+    :return: Retorna uma lista com os links de todas as letras da banda. 
+    """  
     site = requests.get(link).content
     soup = BeautifulSoup(site, 'html.parser')
     links = soup.find_all("a",attrs={"class": "nameMusic"})
@@ -116,75 +147,86 @@ def LinkLetraMusicas(link):
 LinkLetraMusicas("https://www.vagalume.com.br/skillet/discografia/")
 
 
-# A função "LinkLetraMusica" acima não pegou o link por completo, então vamos ter que completar com "https://www.vagalume.com.br"
-# para que o scraping funcione corretemente.
-
-# Função que vai crigar uma lista com o pedaço do link que está faltando 128 vezes, que é a quantidade 
-# de músicas que temos nos álbuns.
 pedaco_que_falta = []
 
 def PedacoFaltando(pedaco):
+    """
+    PedacoFaltando Faz uma lista com o pedaço "https://www.vagalume.com.br" que falta quando pegamos o link para as letras.
+    :param pedaco: Parte do link que falta.
+    :return: Retorna uma lista com o pedaço do link que falta repetido 128 vezes, ou seja a quantidade de músicas. 
+    """ 
     for i in range(0,128):
         pedaco_que_falta.append(pedaco)
+
     return pedaco_que_falta
 PedacoFaltando("https://www.vagalume.com.br")
 
 
-# Função que vai juntar o pedaço que falta do link com o restante do link e adicionar em uma lista com os links completos.
 link_letras_completo = []
 
-def CompletandoLink(pedaco_que_falta):    
+def CompletandoLink(pedaco_que_falta):
+    """
+    CompletandoLink completo o link para música.
+    :param pedaco_que_falta: Lista com o pedaço que falta repetido 128 vezes.
+    :return: Retorna uma lista com o link completo para as letras das músicas. 
+    """     
     for n1, n2 in zip(pedaco_que_falta, link_para_as_letras):
         link_letras_completo.append(n1 + n2)
+
     return link_letras_completo
 CompletandoLink(pedaco_que_falta)
 
 
-
-# Função que vai fazer o scraping das letras das músicas e adicionar em uma lista.
 letras_das_musicas = []
 
 def LetrasMusicas(link):
+    """
+    LetrasMusicas pega a letra de quase todas as músicas da banda skillet.
+    :param link: link da letra.
+    :return: Retorna uma lista com as letras das músicas. 
+    """     
     site = requests.get(link).content
     soup = BeautifulSoup(site, 'html.parser')
     letras_musicas = soup.find_all("div", attrs={"id": "lyrics"})     
     
     for letra_de_musica in letras_musicas:
         letras_das_musicas.append(letra_de_musica.get_text(" "))
+        
     return letras_das_musicas
 
 
-# Função que vai percorrer link_letras_completo e para cada link nessa lista executar a função "LetrasMusicas"
-# e no final teremos uma lista com QUASE todas as letras das músicas.
+# Pegando cada link de letra de música e executando a função LetrasMusicas para pegar a letra de quase todas as letras.
+for link in link_letras_completo:
+    LetrasMusicas(link)
 
-def QuaseTodasAsLetras(links):
-    for link in links:
-        LetrasMusicas(link)
-QuaseTodasAsLetras(link_letras_completo)
-
-
-# Repare na palavra "quase" em destaque anteriormente, isso se deve ao fato de que temos uma música que sua letra
-# é colocada no site de uma forma diferente das demais, e por isso vamos ter que fazer um scraping separado para essa
-# letra e depois apenas vamos adicionala no lugar que ela deveria ser originalmente, que sabemos apenas observando o site.
-
-# Função que vai pegar a letra da musica gasoline que falta.
 
 gasoline_letra_completa = []
+
 def GasolineLetra(link):
+    """
+    GasolineLetra, a música gasoline está disposta no site de forma diferente, por isso temos que fazer o scraping da sua letra separadamente.
+    :param link: link da letra da música gasoline.
+    :return: Retorna uma lista com a letra da música gasoline. 
+    """
     site = requests.get(link).content
     soup = BeautifulSoup(site,'html.parser')
     letra_gasoline = soup.find_all("div",attrs={"id": "lyrics"})
+    
     for parte in letra_gasoline:
         gasoline_letra_completa.append(parte.text)
+    
     return gasoline_letra_completa
 GasolineLetra("https://www.vagalume.com.br/skillet/gasoline.html")
 
-# Função que adiciona a letra da musica gasoline no lugar correto
+
 def GasolineNoLugar(posicao):
+    """
+    GasolineNoLugar coloca a letra da música no lugar correto, que sabemos pelo site onde elas se encontram.
+    :param posicao: posição em que a música gasoline se encontra no site.
+    :return: Adiciona a letra da música gasoline na lista de letras no lugar correto. 
+    """
     letras_das_musicas.insert(posicao, gasoline_letra_completa[0])
 GasolineNoLugar(26)
-
-
 
 
 duracao_das_musicas =  []
@@ -260,6 +302,7 @@ def DuracaoAwake(link):
     return duracao_das_musicas
 DuracaoAwake("https://music.apple.com/br/album/awake/326077729")
 
+
 def DuracaoComatose(link):
     """
     DuracaoComatose encontra a duração de cada música do álbum Comatose
@@ -275,6 +318,7 @@ def DuracaoComatose(link):
 
     return duracao_das_musicas
 DuracaoComatose("https://music.apple.com/br/album/comatose/193381606")
+
 
 def DuracaoCollide(link):
     """
@@ -313,6 +357,7 @@ def DuracaoAlien(link):
     return duracao_das_musicas
 DuracaoAlien("https://music.apple.com/br/album/alien-youth/1502167776")
 
+
 def DuracaoArdent(link):
     """
     DuracaoArdent encontra a duração de cada música do álbum Ardent
@@ -329,6 +374,7 @@ def DuracaoArdent(link):
     return duracao_das_musicas
 DuracaoArdent("https://en.wikipedia.org/wiki/Ardent_Worship")
 
+
 def DuracaoInvencible(link):
     """
     DuracaoInvencible encontra a duração de cada música do álbum Invencible
@@ -341,6 +387,7 @@ def DuracaoInvencible(link):
     
     for duracao in tempo_duracao:
         duracao_das_musicas.append(duracao.get_text(strip=True))
+
     return duracao_das_musicas
 DuracaoInvencible("https://music.apple.com/br/album/invincible/1502265525")
 
@@ -360,6 +407,7 @@ def DuracaoHey(link):
         
     return duracao_das_musicas
 DuracaoHey("https://music.apple.com/br/album/hey-you-i-love-your-soul/1502163392")
+
 
 def DuracaoSkillet(link):
     """
@@ -382,7 +430,9 @@ DuracaoSkillet("https://music.apple.com/br/album/skillet/1502161585")
 client_credentials_manager = SpotifyClientCredentials(client_id="bd17a12d5a91497d96e1390cfe2a96bd", client_secret="f300c3af0a6e4793bf72a57bd95fec18") 
 sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
+
 tracks_id = []
+
 def TracksID(album_id):
     """
     TracksID encontra o id de todas as músicas do álbum
@@ -409,7 +459,9 @@ albums_id = ["1ZUW7enyVvaUZgkcWXk6wG?si=hAxK4BTHSqGGL0m_PTf64Q","4rJu3hBFAUqIAQO
 for album_id in albums_id:
     TracksID(album_id)      
 
+
 popularity = []
+
 def Popularidade(id):
     """
     Popularidade encontra a popularidade de cada música de um álbum no spotfy
@@ -421,8 +473,10 @@ def Popularidade(id):
 
     return popularity
 
-for track_id in tracks_id:
-    Popularidade(track_id)
+
+# Executando a função Popularidade para cada link de música.
+for id in tracks_id:
+    Popularidade(id)
 
 # No spotfy, em um dos álbuns temos no álbum collide quando coletamos, aqui retiramos a popularidade dessa música
 del popularity[64]

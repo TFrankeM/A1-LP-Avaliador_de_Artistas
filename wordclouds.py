1b0e321cc7946308eef72dc51fbc8a0b9c794c04
Teago <thiago.poganski@gmail.com>
2022-10-08 16:03:32 -0300

    Adicionar wordclouds

--- wordclouds.py
+++ wordclouds.py
@@ -4,7 +4,6 @@ import numpy as np
 import interface as it
 import perguntas as pg
 
-
 # Banco de dados visualizado
 arquivo = 'BD - Skillet.xlsx'
 # Recebe apenas o dataframe, sem acionar o módulo de perguntas.
